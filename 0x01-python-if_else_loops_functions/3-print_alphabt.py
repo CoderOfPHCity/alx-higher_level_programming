a0829dbd70f5f171d847a77c499af2c3b7575194
Daniel from Phcity <103477809+CoderOfPHCity@users.noreply.github.com>
2022-08-25 00:16:28 +0100

    Update 3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-for c in range(ord('a'), ord('z')+1):
-    if c is not (ord('q')) and c is not (ord('e')):
-        print('{}'.format(chr(c)), end='')
+for char in range(26):
+    print("{:s}".format(chr(char + ord("a"))), end="")
