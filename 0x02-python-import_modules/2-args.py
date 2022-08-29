4b9790b552ebe2d73ede17b7967414662421da46
Daniel from Phcity <103477809+CoderOfPHCity@users.noreply.github.com>
2022-08-29 17:49:14 +0100

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python3
+from sys import argv
+
+
+def main():
+    number_of_arg = len(argv) - 1
+    if number_of_arg < 1:
+        print("{} arguments.".format(number_of_arg))
+    elif number_of_arg == 1:
+        print("{} argument:".format(number_of_arg))
+        print("{}: ".format(number_of_arg) + argv[1])
+    elif number_of_arg > 1:
+        print("{} arguments:".format(number_of_arg))
+        for i in range(1, len(argv)):
+            print('{}: {}'.format(i, argv[i]))
+
 
 if __name__ == "__main__":
-    from sys import argv
-
-    if len(argv) == 1:
-        print("{} arguments.".format(len(argv) - 1))
-    elif len(argv) == 2:
-        print("{} arguments:".format(len(argv) - 1))
-        for i in range(1, len(argv)):
-            print('{}: {}'.format(i, argv[i]))
-    else:
-        print("{} arguments:".format(len(argv) - 1))
-        for i in range(1, len(argv)):
-            print('{}: {}'.format(i, argv[i]))
+    main()
