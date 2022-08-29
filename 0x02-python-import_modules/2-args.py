ee3132dc60dc55d0a2da1def32cbd12a328a3ce7
Daniel from Phcity <103477809+CoderOfPHCity@users.noreply.github.com>
2022-08-29 17:46:08 +0100

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
     from sys import argv
-    print("{} argument".format(len(argv) - 1), end="")
-    if len(argv) - 1 == 0:
-        print("s.")
-    else:
-        if len(argv) - 1 == 1:
-            print(":")
-        else:
-            print("s:")
+
+    if len(argv) == 1:
+        print("{} arguments.".format(len(argv) - 1))
+    elif len(argv) == 2:
+        print("{} arguments:".format(len(argv) - 1))
         for i in range(1, len(argv)):
-            print("{}: {}".format(i, argv[i]))
+            print('{}: {}'.format(i, argv[i]))
+    else:
+        print("{} arguments:".format(len(argv) - 1))
+        for i in range(1, len(argv)):
+            print('{}: {}'.format(i, argv[i]))
